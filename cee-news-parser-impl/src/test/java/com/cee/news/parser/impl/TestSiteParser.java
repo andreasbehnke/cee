7379d6b420b9953830d00bd8b3c1dab453f0d610
andreasbehnke <andreasbehnke@e4e58025-7ab8-4144-886e-34252af762b1>
2011-06-07 19:42:42 +0000

    fixed illegal character and formatting problem

--- cee-news-parser-impl/src/test/java/com/cee/news/parser/impl/TestSiteParser.java
+++ cee-news-parser-impl/src/test/java/com/cee/news/parser/impl/TestSiteParser.java
@@ -18,32 +18,39 @@ import com.cee.news.parser.net.WebResponse;
 
 public class TestSiteParser {
 
-    @Test
-    public void testParse() throws IOException, SAXException {
-        URL siteLocation = getClass().getResource("spiegel.html");
-        WebClient webClient = new WebClient() {
-            public InputStream openStream(URL location) throws IOException {
-                return location.openStream();
-            }
+	@Test
+	public void testParse() throws IOException, SAXException {
+		URL siteLocation = getClass().getResource("spiegel.html");
+		WebClient webClient = new WebClient() {
+			public InputStream openStream(URL location) throws IOException {
+				return location.openStream();
+			}
 
-            public WebResponse openWebResponse(URL location) {
-                return null;
-            }
-        };
-        SiteParser parser = new SiteParser(new Parser(), new RomeFeedChecker(webClient), webClient);
-        Site site = parser.parse(siteLocation);
-        assertEquals("SPIEGEL ONLINE - Nachrichten", site.getTitle());
-        assertTrue(site.getDescription().startsWith("Deutschlands f"));
-        assertEquals(2, site.getFeeds().size());
-        Feed feed = site.getFeeds().get(0);
-        assertEquals("SPIEGEL ONLINE RSS Schlagzeilen", feed.getTitle());
-        assertEquals("application/rss+xml", feed.getContentType());
-        assertEquals(new URL(siteLocation, "spiegelSchlagzeilen.rss").toExternalForm(), feed.getLocation());
-        feed = site.getFeeds().get(1);
-        assertEquals("SPIEGEL ONLINE RSS Nachrichten", feed.getTitle());
-        assertEquals("application/rss+xml", feed.getContentType());
-        assertEquals(new URL(siteLocation, "spiegelNachrichten.rss").toExternalForm(), feed.getLocation());
-        
-    }
+			public WebResponse openWebResponse(URL location) {
+				return null;
+			}
+		};
+		SiteParser parser = new SiteParser(new Parser(), new RomeFeedChecker(
+				webClient), webClient);
+		Site site = parser.parse(siteLocation);
+		assertEquals("SPIEGEL ONLINE - Nachrichten", site.getTitle());
+		assertTrue(site.getDescription().startsWith("Deutschlands f"));
+		assertEquals(2, site.getFeeds().size());
+		Feed feed = site.getFeeds().get(0);
+		assertEquals("SPIEGEL ONLINE RSS Schlagzeilen", feed.getTitle());
+		assertEquals("application/rss+xml", feed.getContentType());
+		assertEquals(
+				new URL(siteLocation, "spiegelSchlagzeilen.rss")
+						.toExternalForm(),
+				feed.getLocation());
+		feed = site.getFeeds().get(1);
+		assertEquals("SPIEGEL ONLINE RSS Nachrichten", feed.getTitle());
+		assertEquals("application/rss+xml", feed.getContentType());
+		assertEquals(
+				new URL(siteLocation, "spiegelNachrichten.rss")
+						.toExternalForm(),
+				feed.getLocation());
+
+	}
 
 }
