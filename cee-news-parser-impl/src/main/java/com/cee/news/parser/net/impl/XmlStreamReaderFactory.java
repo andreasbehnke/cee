93a5e9d3b475c01655f27466257f4a5eb971c7ea
andreasbehnke <andreasbehnke@e4e58025-7ab8-4144-886e-34252af762b1>
2011-10-28 20:49:34 +0000

    - switched to XmlStreamReader implementation of commons-io - better log4j logging configuration

--- cee-news-parser-impl/src/main/java/com/cee/news/parser/net/impl/XmlStreamReaderFactory.java
+++ cee-news-parser-impl/src/main/java/com/cee/news/parser/net/impl/XmlStreamReaderFactory.java
@@ -5,13 +5,13 @@ import java.io.InputStream;
 import java.io.Reader;
 
 import com.cee.news.parser.net.ReaderFactory;
-import com.sun.syndication.io.XmlReader;
+import org.apache.commons.io.input.XmlStreamReader;;
 
 public class XmlStreamReaderFactory implements ReaderFactory {
 
 	@Override
 	public Reader createReader(InputStream inputStream, String contentTypeHint, String characterEncodingHint) throws IOException {
-		return new XmlReader(inputStream, contentTypeHint, true, characterEncodingHint);
+		return new XmlStreamReader(inputStream, contentTypeHint, true, characterEncodingHint);
 	}
 
 }
