9fd7a7e06dade42c20dfb57c7fc6db8bd2557c3f
andreasbehnke <andreasbehnke@e4e58025-7ab8-4144-886e-34252af762b1>
2011-11-11 06:28:58 +0000

    - introduced betamax - Issue 120

--- cee-news-parser-impl/src/test/java/com/cee/news/parser/impl/TestBoilerpipeArticleParser.java
+++ cee-news-parser-impl/src/test/java/com/cee/news/parser/impl/TestBoilerpipeArticleParser.java
@@ -3,10 +3,19 @@ package com.cee.news.parser.impl;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.net.ProxySelector;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.routing.HttpRoutePlanner;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.ProxySelectorRoutePlanner;
 import org.ccil.cowan.tagsoup.Parser;
+import org.junit.Rule;
 import org.junit.Test;
+
+import betamax.Betamax;
+import betamax.Recorder;
+import betamax.TapeMode;
 
 import com.cee.news.model.Article;
 import com.cee.news.parser.ArticleParser;
@@ -16,9 +25,22 @@ import com.cee.news.parser.net.impl.DefaultWebClient;
 import com.cee.news.parser.net.impl.XmlStreamReaderFactory;
 
 public class TestBoilerpipeArticleParser {
+	
+	@Rule
+	public Recorder recorder = new Recorder();
 
     private static final String ARTICLE_START_TEXT = "Russischer ";
 	private static final String ARTICLE_LOCATION = "http://www.test.com/com/cee/news/parser/impl/spiegelArticle.html";
+	
+	protected HttpClient createHttpClient() {
+		DefaultHttpClient client = new DefaultHttpClient();
+		HttpRoutePlanner routePlanner = new ProxySelectorRoutePlanner(
+		    client.getConnectionManager().getSchemeRegistry(),
+		    ProxySelector.getDefault()
+		);
+		client.setRoutePlanner(routePlanner);
+		return client;
+	}
 
 	@Test
     public void testParse() throws ParserException, IOException {
@@ -29,11 +51,13 @@ public class TestBoilerpipeArticleParser {
         assertTrue(article.getContent().get(0).getContent().startsWith(ARTICLE_START_TEXT));
     }
 
+	@Betamax(tape = "issue120", mode = TapeMode.READ_ONLY)
 	@Test
 	public void testParseRegressionIssue120() throws ParserException, IOException {
 		Article article = new Article();
         article.setLocation("http://www.swr.de/nachrichten/-/id=396/nid=396/did=8825882/1u2s8qj/index.html");
-        ArticleParser parser = new BoilerpipeArticleParser(new Parser(), new DefaultWebClient(new DefaultHttpClient(), new XmlStreamReaderFactory()));
+        
+        ArticleParser parser = new BoilerpipeArticleParser(new Parser(), new DefaultWebClient(createHttpClient(), new XmlStreamReaderFactory()));
         parser.parse(article);
         assertTrue(article.getContent().get(0).getContent().contains("Kein Referendum, kein Rücktritt, keine Lösung"));
 	}
