ae29a2dc9328a2aac261b72e71eac950e8345e73
andreasbehnke <andreasbehnke@e4e58025-7ab8-4144-886e-34252af762b1>
2012-10-26 13:21:36 +0000

    Removed Betamax dependency, all tests are using class resources for now on

--- cee-news-parser-impl/src/test/java/com/cee/news/parser/impl/TestBoilerpipeArticleParser.java
+++ cee-news-parser-impl/src/test/java/com/cee/news/parser/impl/TestBoilerpipeArticleParser.java
@@ -6,13 +6,7 @@ import java.io.IOException;
 import java.net.URL;
 
 import org.ccil.cowan.tagsoup.Parser;
-import org.junit.Ignore;
-import org.junit.Rule;
 import org.junit.Test;
-
-import betamax.Betamax;
-import betamax.Recorder;
-import betamax.TapeMode;
 
 import com.cee.news.model.Article;
 import com.cee.news.parser.ArticleParser;
@@ -23,9 +17,6 @@ import com.cee.news.parser.net.impl.XmlStreamReaderFactory;
 
 public class TestBoilerpipeArticleParser {
 	
-	@Rule
-	public Recorder recorder = new Recorder();
-
 	private boolean testExpectedContent(URL articleLocation, String... expectedContents) throws ParserException, IOException {
 		Article article = new Article();
         article.setLocation(articleLocation.toExternalForm());
@@ -46,28 +37,21 @@ public class TestBoilerpipeArticleParser {
 				getClass().getResource("spiegelArticle.html"), 
 				"Russischer "));
     }
-
-	@Betamax(tape = "issue120", mode = TapeMode.READ_ONLY)
-	@Test
-	public void testParseRegressionIssue120() throws ParserException, IOException {
-		Article article = new Article();
-        article.setLocation("http://www.swr.de/nachrichten/-/id=396/nid=396/did=8825882/1u2s8qj/index.html");
-        
-        ArticleParser parser = new BoilerpipeArticleParser(new Parser(), new DefaultWebClient(new DefaultHttpClientFactory(), new XmlStreamReaderFactory()));
-        parser.parse(article);
-        assertTrue(article.getContentText().contains("Kein Referendum, kein Rücktritt, keine Lösung"));
-	}
 	
-	@Betamax(tape = "issue144", mode = TapeMode.READ_ONLY)
 	@Test
 	public void testParseRegressionIssue144() throws ParserException, IOException {
-		Article article = new Article();
-        article.setLocation("http://www.spiegel.de/wirtschaft/unternehmen/0,1518,797729,00.html");
-        
-        ArticleParser parser = new BoilerpipeArticleParser(new Parser(), new DefaultWebClient(new DefaultHttpClientFactory(), new XmlStreamReaderFactory()));
-        parser.parse(article);
-        assertTrue(article.getContentText().contains("die Polizei durchsucht das Büro"));
+		assertTrue(testExpectedContent(
+				getClass().getResource("issue144.html"), 
+				"die Polizei durchsucht das Büro"));
 	}
+	
+	@Test
+	public void testParseRegressionIssue146() throws ParserException, IOException {
+		assertTrue(testExpectedContent(
+				getClass().getResource("issue146.html"), 
+				"Die Portugiesen selbst sind weniger optimistisch"));
+	}
+	
 	
     @Test
     public void testParseRegressionIssue205() throws ParserException, IOException {
@@ -88,25 +72,6 @@ public class TestBoilerpipeArticleParser {
 				"In mehreren Werbefilmen, die am Freitag im pakistanischen Fernsehen ausgestrahlt wurden, distanzieren sich der amerikanische Präsident Barack Obama und Außenministerin Hillary Clinton von dem islamfeindlichen Film",
 				"Das Innenministerium in Tunis hatte zuvor Demonstrationen gegen den Film verboten"));
     }
-	
-	@Ignore("The server does not send a UTF-8 Content-Type header, betamax is also unable to detect the charset encoding...")
-	//@Betamax(tape = "issue145", mode = TapeMode.READ_WRITE)
-	@Test
-	public void testParseRegressionIssue145() throws ParserException, IOException {
-		Article article = new Article();
-        article.setLocation("http://www.swr.de/nachrichten/-/id=396/nid=396/did=8892142/1aevpcg/index.html");
-        
-        ArticleParser parser = new BoilerpipeArticleParser(new Parser(), new DefaultWebClient(new DefaultHttpClientFactory(), new XmlStreamReaderFactory()));
-        parser.parse(article);
-        assertTrue(article.getContent().get(0).getContent().contains("täglich"));
-	}
-	
-	@Test
-	public void testParseRegressionIssue146() throws ParserException, IOException {
-		assertTrue(testExpectedContent(
-				getClass().getResource("issue146.html"), 
-				"Die Portugiesen selbst sind weniger optimistisch"));
-	}
 	
 	@Test
 	public void testParseRegressionIssue212() throws ParserException, IOException {
--- cee-news-parser-impl/src/test/java/com/cee/news/parser/impl/TestRomeFeedParser.java
+++ cee-news-parser-impl/src/test/java/com/cee/news/parser/impl/TestRomeFeedParser.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Calendar;
 import java.util.List;
 
