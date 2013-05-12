17461adefdc7f9b1712b37f1c686d86739baf06a
andreasbehnke <andreasbehnke@e4e58025-7ab8-4144-886e-34252af762b1>
2013-05-12 10:34:09 +0000

    ASSIGNED - # 269: Split Spring Configuration, optimize scopes http://www.andreasbehnke.com/mantis/view.php?id=269

--- cee-news-webreader/src/test/java/com/cee/news/server/content/TestSpringConfiguration.java
+++ cee-news-webreader/src/test/java/com/cee/news/server/content/TestSpringConfiguration.java
@@ -1,15 +1,25 @@
 package com.cee.news.server.content;
 
-import org.junit.Ignore;
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 import com.sourceallies.beanoh.BeanohTestCase;
 
-@Ignore
 public class TestSpringConfiguration extends BeanohTestCase {
 	
+	private final static String TEST_REPOSITORY_DIR = "target/repository1";
+	
 	@Test
-    public void testContext() {  
-        assertContextLoading();
+    public void testContext() throws IOException {
+		try {
+			assertContextLoading();
+		} finally {
+			File repdir = new File(TEST_REPOSITORY_DIR);
+			System.out.println("deleting repository directory: " + repdir.getAbsolutePath());
+			FileUtils.deleteDirectory(repdir);
+		}
     }
 }
