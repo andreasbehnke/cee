7010012a977c925c00269f09509067ca53e9ed7d
andreasbehnke <andreasbehnke@e4e58025-7ab8-4144-886e-34252af762b1>
2013-07-06 19:34:26 +0000

    Incomplete - task : Renderer should not render empty or null content

---
+++ cee-news-webreader/src/main/java/com/cee/news/server/content/renderer/HtmlBuilder.java
@@ -0,0 +1,58 @@
+package com.cee.news.server.content.renderer;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class HtmlBuilder {
+	
+	private final StringBuilder buffer;
+	
+	public HtmlBuilder(final StringBuilder buffer) {
+	    this.buffer = buffer;
+    }
+
+	private String formatDate(Calendar calendar) {
+        return SimpleDateFormat.getDateInstance().format(calendar.getTime());
+    }
+
+    public HtmlBuilder appendHtmlElement(String htmlElement, Object content) {
+    	buffer.append('<').append(htmlElement).append('>')
+			.append(content)
+			.append("</").append(htmlElement).append('>');
+    	return this;
+    }
+    
+    public HtmlBuilder appendIfNotNull(String htmlElement, Object content) {
+    	if (content != null) {
+    		appendHtmlElement(htmlElement, content);
+    	}
+    	return this;
+    }
+    
+    public HtmlBuilder appendIfNotNull(String htmlElement, Calendar calendar) {
+    	if (calendar != null) {
+    		appendHtmlElement(htmlElement, formatDate(calendar));
+    	}
+    	return this;
+    }
+    
+    public HtmlBuilder appendIfIsNumber(String htmlElement, double number) {
+    	if (number > 0) {
+    		appendHtmlElement(htmlElement, number);
+    	}
+    	return this;
+    }
+    
+    public HtmlBuilder appendLink(String href, String target, String content) {
+    	buffer.append("<a href=\"").append(href).append("\" target=\"").append(target).append("\">")
+    		.append(content).append("</a>");
+    	return this;
+    }
+    
+    public HtmlBuilder append(Object content)  {
+    	buffer.append(content);
+    	return this;
+    }
+    
+    
+}
