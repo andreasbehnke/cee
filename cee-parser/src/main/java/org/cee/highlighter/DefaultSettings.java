8639bb19be619a20a4770f51f697bcff33cbaf5a
andreas <andreas@bla.de>
2013-10-07 21:31:14 +0200

    fixed default template resource path

--- cee-parser/src/main/java/org/cee/highlighter/DefaultSettings.java
+++ cee-parser/src/main/java/org/cee/highlighter/DefaultSettings.java
@@ -6,14 +6,16 @@ import org.cee.highlighter.ContentHighlighter.Settings;
 
 public class DefaultSettings extends Settings {
 	
-	public static final String DEFAULT_HEADER_TEMPLATE = "classpath:/com/cee/news/highlighter/defaultHeaderTemplate.html";
+	public static final String DEFAULT_HEADER_TEMPLATE = "classpath:/org/cee/highlighter/defaultHeaderTemplate.html";
 	public static final boolean DEFAULT_HIGHLIGHT_CONTENT_BLOCK = true;
-	public static final String DEFAULT_CONTENT_BLOCK_TEMPLATE = "classpath:/com/cee/news/highlighter/defaultContentBlockTemplate.html";
+	public static final String DEFAULT_CONTENT_BLOCK_TEMPLATE = "classpath:/org/cee/highlighter/defaultContentBlockTemplate.html";
 	public static final boolean DEFAULT_REWRITE_URLS = true;
 	public static final boolean DEFAULT_SHOW_BLOCK_METADATA = true;
-	public static final String DEFAULT_METADATA_TEMPLATE = "classpath:/com/cee/news/highlighter/defaultMetadataTemplate.html";
-	public static final String DEFAULT_METADATA_CONTAINER_TEMPLATE = "classpath:/com/cee/news/highlighter/defaultMetadataContainerTemplate.html";
-	public static final String DEFAULT_METADATA_ICON_TEMPLATE = "classpath:/com/cee/news/highlighter/defaultMetadataIconTemplate.html";
+	public static final String DEFAULT_METADATA_TEMPLATE = "classpath:/org/cee/highlighter/defaultMetadataTemplate.html";
+	public static final String DEFAULT_METADATA_CONTAINER_TEMPLATE = "classpath:/org/cee/highlighter/defaultMetadataContainerTemplate.html";
+	public static final String DEFAULT_METADATA_ICON_TEMPLATE = "classpath:/org/cee/highlighter/defaultMetadataIconTemplate.html";
+	
+	private DefaultSettings() {}
 	
 	public static Settings createDefaultSettings(URL baseUrl) {
 		return  new Settings(
