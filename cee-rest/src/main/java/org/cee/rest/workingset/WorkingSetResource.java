4d91d9218a4a3169f3637b377a9396942a4ddac6
Andreas Behnke <ab@andreasbehnke.com>
2015-01-08 20:27:12 +0100

    Implemented simple REST API for WorkingSet

---
+++ cee-rest/src/main/java/org/cee/rest/workingset/WorkingSetResource.java
@@ -0,0 +1,41 @@
+package org.cee.rest.workingset;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.cee.client.workingset.WorkingSetData;
+import org.cee.service.workingset.WorkingSetService;
+import org.cee.store.EntityKey;
+import org.cee.store.StoreException;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Component;
+
+@Path("workingset")
+@Component
+public class WorkingSetResource {
+	
+	private WorkingSetService workingSetService;
+	
+	@Required
+	public void setWorkingSetService(WorkingSetService workingSetService) {
+		this.workingSetService = workingSetService;
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<EntityKey> orderedByName() throws StoreException {
+		return workingSetService.orderedByName();
+	}
+	
+	@GET
+	@Path("{key}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public WorkingSetData get(@PathParam("key") String key) throws StoreException {
+		return workingSetService.get(EntityKey.get(key));
+	}
+}
