6c9d3c27f6350fb0b64ffa613e2944190a1904b1
Andreas Behnke <ab@andreasbehnke.com>
2015-01-20 19:08:57 +0100

    Implemented language REST resource

---
+++ cee-rest/src/main/java/org/cee/rest/language/LanguageResource.java
@@ -0,0 +1,30 @@
+package org.cee.rest.language;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.cee.client.language.LanguageList;
+import org.cee.rest.BaseResource;
+import org.cee.service.EntityNotFoundException;
+import org.cee.service.language.LanguageService;
+import org.cee.store.StoreException;
+import org.springframework.stereotype.Component;
+
+@Path("language")
+@Produces(MediaType.APPLICATION_JSON)
+@Component
+public class LanguageResource extends BaseResource {
+
+	private LanguageService languageService;
+	
+	public void setLanguageService(LanguageService languageService) {
+		this.languageService = languageService;
+	}
+	
+	@GET
+	public LanguageList supportedLanguages() throws StoreException, EntityNotFoundException {
+		return languageService.getSupportedLanguages();
+	}
+}
