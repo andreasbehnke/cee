f66dbbf665020e2a1e45e98bd63cb16f4ca2ec2e
Andreas Behnke <ab@andreasbehnke.com>
2015-03-19 08:36:01 +0100

    Initial commit angular web client

---
+++ cee-research-client/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ceeResearchClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ceeResearchClientApp
+ */
+angular.module('ceeResearchClientApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
